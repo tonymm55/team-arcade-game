548d578412cabb078d2de1658e12e2688c16be7a
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-05-03 21:13:39 +0100

    began styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import soundOn from "../assets/Site/soundon.png";
+import soundOff from "../assets/Site/soundoff.png";
+import "../styles/Footer.css";
+
 const Footer = () => {
   return (
     <footer className="footer" id="footer">
       <p>footer</p>
+      <img src={soundOn} alt="sound on icon" className="footer__image-aspect" />
+      <img
+        src={soundOff}
+        alt="sound off icon"
+        className="footer__image-aspect"
+      />
     </footer>
   );
 };
