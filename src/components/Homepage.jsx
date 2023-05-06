2a5da4e5b7e7fa65028d5e3c480bba7546a292bd
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-05-06 10:51:37 +0100

    added swiper container

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,19 @@
+import { register } from "swiper/element/bundle";
+import "../styles/Homepage.css";
+
+register();
+
 const Homepage = () => {
-  return <h2>Homepage Component</h2>;
+  return (
+    <main className="homepage">
+      <h2>Homepage Component</h2>
+      <swiper-container slides-per-view="1" navigation="true" pagination="true">
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+      </swiper-container>
+    </main>
+  );
 };
 
 export default Homepage;
