14a049b10fb94391395de4ac98a0be2a983d6317
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-05-06 13:29:32 +0100

    added gamepage component

---
+++ src/components/Gamepage.jsx
@@ -0,0 +1,14 @@
+const Gamepage = () => {
+  return (
+    <>
+      <h2>Game Title</h2>
+      <p>Pass in data as props: </p>
+      <p>Highscores</p>
+      <p>Description</p>
+      <p>image background</p>
+      <button type="button">Start Game</button>
+    </>
+  );
+};
+
+export default Gamepage;
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,12 @@
 import { register } from "swiper/element/bundle";
 import "../styles/Homepage.css";
+import Gamepage from "./Gamepage";
 
 register();
 
 const Homepage = () => {
   // make reusable component and pass in props to make the indivdual slides for games
+  // put game data (title description) in .json pull in and pass into gamepage as props
   return (
     <main className="homepage">
       <h2>Homepage Component</h2>
@@ -17,7 +19,9 @@ const Homepage = () => {
           //   pagination="true"
           thumbs-swiper=".homepage__thumbs"
         >
-          <swiper-slide>Game 1</swiper-slide>
+          <swiper-slide>
+            <Gamepage />
+          </swiper-slide>
           <swiper-slide>Game 2</swiper-slide>
           <swiper-slide>Game 3</swiper-slide>
           <swiper-slide>Game 4</swiper-slide>
