eacf715445e38345ec19b0f2d1da845339ead714
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-05-06 15:10:01 +0100

    minor file changes

--- src/components/RunnerGame/RunnerGame.jsx
+++ src/components/RunnerGame/RunnerGame.jsx
@@ -6,6 +6,10 @@ const RunnerGame = () => {
       <h1>Runner Game</h1>
       <RunnerParallaxBackground />
       <p>Return to Game Select</p>
+      <p>
+        This will be a separate component that will replace the caraousel when
+        selected, Ive left it here so its more convenient for me to work on
+      </p>
     </div>
   );
 };
