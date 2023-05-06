5cef34938e8cafe6327c806258d90ba11dd68774
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-05-06 12:33:58 +0100

    styled swiper

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,14 +4,36 @@ import "../styles/Homepage.css";
 register();
 
 const Homepage = () => {
+  // make reusable component and pass in props to make the indivdual slides for games
   return (
     <main className="homepage">
       <h2>Homepage Component</h2>
-      <swiper-container slides-per-view="1" navigation="true" pagination="true">
-        <swiper-slide>Slide 1</swiper-slide>
-        <swiper-slide>Slide 2</swiper-slide>
-        <swiper-slide>Slide 3</swiper-slide>
-      </swiper-container>
+      <h3>log in component</h3>
+      <div className="homepage__swiper">
+        <swiper-container
+          class="homepage__swiper-container"
+          slides-per-view="1"
+          navigation="true"
+          //   pagination="true"
+          thumbs-swiper=".homepage__thumbs"
+        >
+          <swiper-slide>Game 1</swiper-slide>
+          <swiper-slide>Game 2</swiper-slide>
+          <swiper-slide>Game 3</swiper-slide>
+          <swiper-slide>Game 4</swiper-slide>
+        </swiper-container>
+        <swiper-container
+          class="homepage__thumbs"
+          slides-per-view="4"
+          watch-slides-progress="true"
+          space-between={10}
+        >
+          <swiper-slide>Thumb 1</swiper-slide>
+          <swiper-slide>Thumb 2</swiper-slide>
+          <swiper-slide>Thumb 3</swiper-slide>
+          <swiper-slide>Thumb 4</swiper-slide>
+        </swiper-container>
+      </div>
     </main>
   );
 };
