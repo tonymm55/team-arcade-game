b32d3e6dcf9992283960786a59c343bc570266c5
Matthew Giles <matty92g@hotmail.co.uk>
2023-05-14 20:36:58 +0100

    Render Game or Nickname if no nickname given. Would like to re-render when entered nickname

--- src/components/Games/RunnerEmbed.jsx
+++ src/components/Games/RunnerEmbed.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-// import RunnerScoreboard from '../Scoreboards/RunnerScoreboard';
+import NickName from '../authentication/NickName';
 
 const RunnerEmbed = () => {
   const [gameScore, setGameScore] = useState(null);
-
+  // const [renderNickname, setRenderNickname] = useState();
   useEffect(() => {
     function handleMessage(event) {
       if (event.origin !== 'https://arcade-game-runner.netlify.app') {
@@ -15,13 +15,16 @@ const RunnerEmbed = () => {
       if (runnerScore) {
         setGameScore(runnerScore);
       }
+
       console.log(localStorage.getItem('nickname'));
       console.log(runnerScore);
+
       const data = {
         name: localStorage.getItem('nickname'),
         score: runnerScore,
         img: localStorage.getItem('profilePic'),
       };
+
       axios
         .post('https://arcade-backend.onrender.com/scoreboard/run/add', data)
         .then((response) => console.log(response))
@@ -36,19 +39,24 @@ const RunnerEmbed = () => {
       window.removeEventListener('message', handleMessage);
     };
   }, [gameScore]);
-
-  return (
-    <>
-      <iframe
-        title="Runner Game"
-        src="https://arcade-game-runner.netlify.app/"
-        width={800}
-        height={720}
-        className="runner-game-embed"
-      ></iframe>
-      {/* <RunnerScoreboard /> */}
-    </>
-  );
+  // setRenderNickname(!localStorage.getItem('nickname') ? false : true);
+  const renderNickname = !localStorage.getItem('nickname') ? false : true;
+  console.log('renderNickname true or false: ', renderNickname);
+  if (renderNickname) {
+    return (
+      <>
+        <iframe
+          title="Runner Game"
+          src="https://arcade-game-runner.netlify.app/"
+          width={800}
+          height={720}
+          className="runner-game-embed"
+        ></iframe>
+      </>
+    );
+  } else {
+    return <NickName />;
+  }
 };
 
 export default RunnerEmbed;
