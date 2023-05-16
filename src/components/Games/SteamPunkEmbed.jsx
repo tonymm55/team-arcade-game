557f1cc2a842969bbc909d27c7c375c2e531f6de
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-05-16 17:36:32 +0100

    added toastify, ninja and steampunk games

---
+++ src/components/Games/SteamPunkEmbed.jsx
@@ -0,0 +1,15 @@
+const SteamPunkEmbed = () => {
+  return (
+    <>
+      <iframe
+        title="Steam Punk Game"
+        className="steam-punk-embed"
+        src="https://steam-punk-project.netlify.app/"
+        width={1000}
+        height={500}
+      ></iframe>
+    </>
+  );
+};
+
+export default SteamPunkEmbed;
