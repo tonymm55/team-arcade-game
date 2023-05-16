3941cf32c068f3167f1cb89f9565dd32af533850
Matthew Giles <matty92g@hotmail.co.uk>
2023-05-16 22:33:10 +0100

    Attempt to create re-usable code for all Games and Scoreboards components

---
+++ src/components/Scoreboards/GameScoreboard.jsx
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const GameScoreboard = ({ Game }) => {
+  const [gameStates, setGameStates] = useState(null);
+
+  const fetchData = async () => {
+    let response;
+    if (Game === 'Run') {
+      response = await axios.get(
+        'https://arcade-backend.onrender.com/scoreboard/run'
+      );
+    } else if (Game === 'Tetris') {
+      response = await axios.get(
+        'https://arcade-backend.onrender.com/scoreboard/tetris'
+      );
+    } else if (Game === 'Steampunk') {
+      response = await axios.get(
+        'https://arcade-backend.onrender.com/scoreboard/steampunk'
+      );
+    }
+    const data = Object.keys(response.data).map((item) => response.data[item]);
+    setGameStates(data);
+  };
+  console.log(gameStates);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const descendingGameStates = gameStates?.sort((a, b) => b.score - a.score);
+  const topTenGameStates = descendingGameStates?.slice(0, 10);
+  return (
+    <div className="score-board">
+      <h3>High Scores: </h3>
+      {topTenGameStates?.map((gameState, index) => (
+        <div className="top-ten-scores" key={index}>
+          <div className="scoreboard-points">
+            <img className="top-ten-scores-image" src={gameState.img} />{' '}
+            <p className="name">{gameState.name}</p>
+            <p className="score">{gameState.score}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GameScoreboard;
