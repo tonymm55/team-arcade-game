efe3e2977b8f11d11321133d8f6cf1fd1d4d7790
Matthew Giles <matty92g@hotmail.co.uk>
2023-05-20 16:05:03 +0100

    Change nickname to h3 element

--- src/components/authentication/SignOut.jsx
+++ src/components/authentication/SignOut.jsx
@@ -14,7 +14,7 @@ const SignOut = () => {
           </div>
           <div className="option-menu">
             <div className="option-menu-trigger">
-              {localStorage.getItem('nickname')}
+              <h3>{localStorage.getItem('nickname')}</h3>
             </div>
             <button
               className="login-with-google-btn"
