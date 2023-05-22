4f64b5e3b2da0e66a623dfa58fdd398a29a02f50
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-05-22 18:48:57 +0100

    added prompt to 3 games

--- src/components/Games/SteamPunkEmbed.jsx
+++ src/components/Games/SteamPunkEmbed.jsx
@@ -16,6 +16,15 @@ const SteamPunkEmbed = () => {
         setGameScore(steamPunkScore);
       }
 
+      if (!localStorage.getItem('nickname')) {
+        const userInput = window.prompt(
+          'Please enter a name to submit your score!'
+        );
+        if (userInput) {
+          localStorage.setItem('nickname', userInput);
+        }
+      }
+
       console.log(localStorage.getItem('nickname'));
       console.log(steamPunkScore);
 
