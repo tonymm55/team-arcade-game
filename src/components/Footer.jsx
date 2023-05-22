73dfc4055b80afb7395eb7f42dd8882d788d9c8a
Matthew Giles <matty92g@hotmail.co.uk>
2023-05-22 19:59:25 +0100

    Switch Sound button image, Mute and Playing text added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,12 +8,19 @@ import { useEffect } from 'react';
 const Footer = () => {
   const [playAudio, setPlayAudio] = useState(false);
   const [music, setMusic] = useState(new Audio(Ambient));
+  const [soundButton, setSoundButton] = useState(soundOff);
+  const [muteState, setMuteState] = useState('Mute');
 
   const handleOnClick = () => {
-    setPlayAudio(true);
-  };
-  const handleOffClick = () => {
-    setPlayAudio(false);
+    if (!playAudio) {
+      setPlayAudio(true);
+      setSoundButton(soundOn);
+      setMuteState('Playing');
+    } else {
+      setPlayAudio(false);
+      setSoundButton(soundOff);
+      setMuteState('Mute');
+    }
   };
 
   useEffect(() => {
@@ -29,20 +36,18 @@ const Footer = () => {
   return (
     <footer className="footer" id="footer">
       <p>footer empty or contains game name</p>
-      <button onClick={handleOnClick}>
-        <img
-          src={soundOn}
-          alt="sound on icon"
-          className="footer__image-aspect"
-        />
-      </button>
-      <button onClick={handleOffClick}>
-        <img
-          src={soundOff}
-          alt="sound off icon"
-          className="footer__image-aspect"
-        />
-      </button>
+      <div className="mute">
+        <button onClick={handleOnClick}>
+          <div className="mute-image">
+            <img
+              src={soundButton}
+              alt="sound icon"
+              className="footer__image-aspect"
+            />
+            <div className="centered">{muteState}</div>
+          </div>
+        </button>
+      </div>
     </footer>
   );
 };
